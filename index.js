172b379ee0e27d5ecef8df635ddb06a4ec9d1ab0
Scyye <github@scyye.dev>
2026-06-12 12:19:38 -0400

    add bruh channel, and close command

--- index.js
+++ index.js
@@ -22,6 +22,36 @@ client.once(Events.ClientReady, (readyClient) => {
 
 client.on(Events.MessageCreate, async (message) => {
     if (message.author.bot) return;
+
+    if (message.channelId === "1512223214984757338" && message.author.id !== client.user.id) {
+        await message.delete().catch(() => {});
+        return;
+    }
+
+    if (message.author.id === "1196149164409639065" && message.content.toLowerCase().includes("bruh")) {
+        await message.react("😂");
+        await message.forward(message.guild.channels.cache.get("1512223214984757338"));
+    }
+
+    if (message.content === "!close") {
+        const channel = message.channel;
+
+        if (channel.parentId === "1514988512582045907")
+
+        // Make sure it's a thread (forum posts = threads)
+        if (!channel.isThread()) return;
+
+        try {
+            await message.react("☑️")
+            await channel.setArchived(true);   // "closes" the forum post
+        } catch (err) {
+            console.error(err);
+            message.reply("Failed to close the thread.");
+        }
+    }
+
+    
+
     if (message.content.startsWith('!eval')) {
         if (!owners.includes(message.author.id)) {
             const reply = await message.reply('This command is strictly for the bot owner(s).');
@@ -32,8 +62,8 @@ client.on(Events.MessageCreate, async (message) => {
             }, 3000);
             return;
         }
-
-        await message.react(message.guild.emojis.cache.get("1511950922815635486"))
+        
+        await message.react("<a:loading:1512087895824138300>");
 
         const codeArg = message.content.slice(5).trim();
         const originalLog = console.log;
@@ -59,7 +89,9 @@ client.on(Events.MessageCreate, async (message) => {
             let evaled = eval(code);
 
             if (evaled instanceof Promise) {
-                evaled = await evaled;
+                evaled = await evaled.catch(err => {
+                    throw new Error(`Promise rejected: ${err}`);
+                });
             }
 
             // Restore console.log
